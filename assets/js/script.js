d0c16cf7921a75f8c1c5a907052febea9869e04b
YUVRAJ KUMAR GOND <gondyuvraj85@gmail.com>
2025-07-29 22:37:59 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -631,3 +631,11 @@ window.addEventListener('resize', function() {
         initParticles();
     }
 });
+window.addEventListener('load', () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+window.addEventListener('resize', () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
